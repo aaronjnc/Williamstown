4855d75cb06dd47cce2bafca7608eaa06f643d53
Aaron <aaronkingnc@att.net>
2020-12-20 17:46:35 -0500

    Added Unity Project

---
+++ Assets/Scripts/ClickAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class ClickAction : MonoBehaviour
+{
+    PlayerControls controls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls = new PlayerControls();
+        controls.BaseActions.Click.performed += OnClick;
+        controls.BaseActions.Enable();
+    }
+    void OnClick(CallbackContext ctx)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 10f);
+        if (hit && hit.transform.gameObject.CompareTag("clickable"))
+        {
+            Debug.Log("clicked");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+using UnityEditor.Animations;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D player;
+    PlayerControls controls;
+    Vector2 direction;
+    public float speed = 5f;
+    public AnimatorController left;
+    public AnimatorController right;
+    Sprite sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>().sprite;
+        player = GetComponent<Rigidbody2D>();
+        controls = new PlayerControls();
+        controls.BaseActions.PlayerMovement.performed += OnMove;
+        controls.BaseActions.PlayerMovement.canceled += Stop;
+        controls.BaseActions.PlayerMovement.Enable();
+    }
+    void OnMove(CallbackContext ctx)
+    {
+        GetComponent<Animator>().enabled = true;
+        direction = ctx.ReadValue<Vector2>();
+        if (direction[0]<1)
+        {
+            GetComponent<Animator>().runtimeAnimatorController = left;
+        }
+        else
+            GetComponent<Animator>().runtimeAnimatorController = right;
+    }
+    void Stop(CallbackContext ctx)
+    {
+        direction = new Vector2(0, 0);
+        GetComponent<Animator>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        player.position += direction*speed*Time.deltaTime;
+    }
+}
