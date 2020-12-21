32c10aa9679452586e7d9cd1d1113d5499633469
Aaron <aaronkingnc@att.net>
2020-12-20 22:25:46 -0500

    Added script to switch between scenes and keep info

--- Assets/Scripts/ClickAction.cs
+++ Assets/Scripts/ClickAction.cs
@@ -7,20 +7,26 @@ using static UnityEngine.InputSystem.InputAction;
 public class ClickAction : MonoBehaviour
 {
     PlayerControls controls;
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
         controls = new PlayerControls();
         controls.BaseActions.Click.performed += OnClick;
         controls.BaseActions.Enable();
     }
+    public int loadtype;
     void OnClick(CallbackContext ctx)
     {
         Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 10f);
         if (hit && hit.transform.gameObject.CompareTag("clickable"))
         {
-            Debug.Log("clicked");
+            NewScene();
         }
+    }
+    void NewScene()
+    {
+        NewScene load = gameObject.AddComponent<NewScene>();
+        load.LoadType(loadtype, gameObject.name);
     }
 }
