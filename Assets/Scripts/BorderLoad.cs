745950003d47e9a4c8605e97ed8a4b57ceb9abae
Aaron <aaronkingnc@att.net>
2021-01-15 17:09:50 -0500

    Added movement between almost all scenes
    Amity boat is janky

---
+++ Assets/Scripts/BorderLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderLoad : MonoBehaviour
+{
+    public Clickinfo.ClickType loadtype;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.gameObject.name.Equals("Agent"))
+        {
+            NewScene newScene = gameObject.AddComponent<NewScene>();
+            newScene.LoadType(loadtype, gameObject.name, collision.transform.position);
+            newScene.LoadScene();
+        }
+    }
+}
