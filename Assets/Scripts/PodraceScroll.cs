c19559ea3f8adbfe83a2b190719fa6e3bc3f0cb0
Aaron <aaronkingnc@att.net>
2021-02-04 00:18:49 -0500

    Added Mos Espa overlay

---
+++ Assets/Scripts/PodraceScroll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodraceScroll : MonoBehaviour
+{
+    Rigidbody2D background;
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Rigidbody2D>();
+        background.velocity = new Vector2(0, -speed);
+    }
+}
