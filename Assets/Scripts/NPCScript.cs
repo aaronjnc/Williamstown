fc3a9b99aad1dfbde3f5279daea703fccade89bf
Aaron <aaronkingnc@att.net>
2021-02-08 17:39:42 -0500

    Added head shots, and basic talking function

---
+++ Assets/Scripts/NPCScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScript : MonoBehaviour
+{
+    public Sprite headshot;
+    string name;
+    void Start()
+    {
+        name = gameObject.name;
+    }
+    public string GetName()
+    {
+        return name;
+    }
+}
